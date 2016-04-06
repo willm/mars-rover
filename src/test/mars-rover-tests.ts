a06fc2be47784f93f75299327b63471032aa81b7
Will Munn <willmunn@hotmail.com>
2016-04-07 00:16:26 +0100

    Refactor tests

--- src/test/mars-rover-tests.ts
+++ src/test/mars-rover-tests.ts
@@ -8,40 +8,40 @@ const upperRightBoundery = { x:1, y: 1};
 test('A robot with no instructions remains at the start position', (assert) => {
     const move = mr.createWorld({ x:0, y:0, orientation: mr.Orientations.North}, upperRightBoundery);
     const finalPosition = move([]);
-    checkPosition(assert, {x: 0, y:0}, finalPosition);
+    checkPosition(assert, {cooridnate: {x: 0, y:0}, lost: false}, finalPosition);
     assert.end();
 });
 
 test('Forward instruction moves robot forward north', (assert) => {
     const move = mr.createWorld({ x:0, y:0, orientation: mr.Orientations.North}, upperRightBoundery);
     const finalPosition = move([mr.Instruction.Forward]);
-    checkPosition(assert, {x: 0, y:1}, finalPosition);
+    checkPosition(assert, {cooridnate: {x: 0, y:1}, lost: false}, finalPosition);
     assert.end();
 });
 
 test('Forward instruction moves robot forward south', (assert) => {
     const move = mr.createWorld({ x:0, y:1, orientation: mr.Orientations.South}, upperRightBoundery);
     const finalPosition = move([mr.Instruction.Forward]);
-    checkPosition(assert, {x: 0, y:0}, finalPosition);
+    checkPosition(assert, {cooridnate: {x: 0, y:0}, lost: false}, finalPosition);
     assert.end();
 });
 
 test('Forward instruction moves robot forward east', (assert) => {
     const move = mr.createWorld({ x:0, y:0, orientation: mr.Orientations.East}, upperRightBoundery);
     const finalPosition = move([mr.Instruction.Forward]);
-    checkPosition(assert, {x: 1, y:0}, finalPosition);
+    checkPosition(assert, {cooridnate: {x: 1, y:0}, lost: false}, finalPosition);
     assert.end();
 });
 
 test('Moving off the grid marks the robot as lost', (assert) => {
     const move = mr.createWorld({ x:0, y:0, orientation: mr.Orientations.East}, upperRightBoundery);
     const finalPosition = move([mr.Instruction.Forward, mr.Instruction.Forward]);
-    checkPosition(assert, {x: 1, y:0}, finalPosition);
-    assert.equal(finalPosition.lost, true);
+    checkPosition(assert, {cooridnate: {x: 1, y:0}, lost: true}, finalPosition);
     assert.end();
 });
 
-function checkPosition (assert:test.Test, expected:mr.Cooridnate, actual:mr.FinalSpot) {
-    assert.equal(expected.x, actual.cooridnate.x);
-    assert.equal(expected.y, actual.cooridnate.y);
+function checkPosition (assert:test.Test, expected:mr.FinalSpot, actual:mr.FinalSpot) {
+    assert.equal(expected.cooridnate.y, actual.cooridnate.y);
+    assert.equal(expected.cooridnate.x, actual.cooridnate.x);
+    assert.equal(expected.lost, actual.lost);
 }
